c09143cafc275f7b4cbc3043f1d654a4f441271a
Kuts Michael <kutsjuice@gmail.com>
2016-06-10 11:38:24 +0300

    Vector3D.__pow__() added and some other function

--- overload.py
+++ overload.py
@@ -9,13 +9,28 @@ class Vector3D():
     def __str__(self):
         return '({},{},{})'.format(self.x,self.y,self.z)
     
+    def __add__(self,other):
+		try:
+			a=Vector3D(self.x+other.x,self.y+other.y,self.z+other.z)
+		except Exception:
+			a=Vector3D(self.x+other,self.y+other,self.z+other)
+		return a
+	
+	def __sub__(self,other):
+		
+		try:
+			a=Vector3D(self.x-other.x,self.y-other.y,self.z-other.z)
+		except Exception:
+			a=Vector3D(self.x-other,self.y-other,self.z-other)
+		return a		
+    
     def __mul__(self,other):
         a=Vector3D()
         try:
             a.x=self.y*other.z-self.z*other.y
             a.y=self.z*other.x-self.x*other.z
             a.z=self.x*other.y-self.y*other.x
-        except:
+        except Exception:
             a.x=self.x*other
             a.y=self.y*other
             a.z=self.z*other
@@ -34,11 +49,10 @@ class Vector3D():
         
         
     def __pow__(self,other):
-        return self.x*other.x+self.y*other.y+self.z+other.z
-        
-#    def rot(self, angle):
-#        return
-     
+		
+        return (self.x*other.x+self.y*other.y+self.z*other.z)
+
+
 if __name__=="__main__":
 	vec=Vector3D(1,1,0)
 	vec2=Vector3D(1,-1,0)
@@ -47,5 +61,4 @@ if __name__=="__main__":
 	print str(vec3),'\n'
 	print str(vec/2.),"\n"
 	print str(vec.norm()),'\n'
-	print str(vec4),'\n'
-	print vec**vec
+	print str(vec4)
